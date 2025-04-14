084a1259ae70c7a7830a10fcf504e9a59699179f
LucaFrioli <lucafrioli7@gmail.com>
2025-04-14 15:17:05 -0300

    feat(arch): mudança na arquitetura

---
+++ frontend/pages/produtos_pages/Moldes.jsx
@@ -0,0 +1,45 @@
+import Header from '../../components/header/header';
+
+function Moldes() {
+	return (
+		<>
+			<Header />
+			<div>
+				<h1>Fabricação de Moldes de Compressão</h1>
+
+				<p>
+					Na Usitop, oferecemos soluções completas e de alta precisão
+					na fabricação de moldes de compressão, com foco na
+					excelência dimensional, durabilidade e eficiência do
+					processo produtivo. Atuamos desde o desenvolvimento do
+					projeto até a entrega final, garantindo a fidelidade ao
+					desenho técnico, a performance do molde e a qualidade da
+					peça final. A moldagem por compressão é amplamente utilizada
+					na fabricação de componentes técnicos em materiais como
+					borracha, termofixos e compostos plásticos reforçados. Essa
+					tecnologia é ideal para a produção de peças com geometrias
+					complexas, alta resistência mecânica e estabilidade térmica,
+					sendo bastante aplicada nos setores automotivo,
+					eletroeletrônico, linha branca e industrial em geral. Nossos
+					moldes são fabricados em aços de alta resistência e, sempre
+					que necessário, passam por rigorosos processos de tratamento
+					térmico para assegurar maior durabilidade e desempenho
+					operacional. Além disso, realizamos o polimento completo das
+					cavidades, proporcionando um acabamento espelhado de alto
+					padrão, que contribui para a qualidade estética e funcional
+					das peças produzidas Contamos com uma equipe técnica
+					qualificada, capaz de desenvolver soluções personalizadas de
+					acordo com a necessidade do cliente, realizando análises de
+					viabilidade, otimização de projeto e validação funcional.
+					Além disso, aplicamos práticas de melhoria contínua em todos
+					os processos, garantindo eficiência, confiabilidade e
+					redução de custos operacionais. Se você busca precisão,
+					agilidade e inovação na fabricação de moldes de compressão,
+					a Usitop é o seu parceiro ideal.
+				</p>
+			</div>
+		</>
+	);
+}
+
+export default Moldes;
