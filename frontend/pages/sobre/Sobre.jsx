ce00b0b2609bdda71b82648c64d3f1b555a42b8b
LucaFrioli <lucafrioli7@gmail.com>
2025-04-14 15:18:02 -0300

    fix(commit): append dos arquivos

--- frontend/pages/sobre/Sobre.jsx
+++ frontend/pages/sobre/Sobre.jsx
@@ -9,14 +9,20 @@ function Sobre() {
 
 			{/* CORPO */}
 			<div class="row my-5 justify-content-center">
-				<img class="col-lg-4 col-md-6 col-10" src="./assets/img/usitop-4.png" alt="Usitop Logo" />
+				<img
+					class="col-lg-4 col-md-6 col-10"
+					src="./assets/img/usitop-4.png"
+					alt="Usitop Logo"
+				/>
 			</div>
 
 			<div class="container-fluid my-5" id="sobre-container">
 				<div class="row mb-5 mt-3 justify-content-center">
 					<p class="lead col-lg-5 col-md-6 text-center">
-						A Usitop é uma empresa comprometida em atender seus clientes com excelência, garantindo o melhor
-						custo-benefício, a máxima qualidade nos serviços prestados e sempre respeitando os prazos.
+						A Usitop é uma empresa comprometida em atender seus
+						clientes com excelência, garantindo o melhor
+						custo-benefício, a máxima qualidade nos serviços
+						prestados e sempre respeitando os prazos.
 					</p>
 				</div>
 
@@ -24,12 +30,21 @@ function Sobre() {
 
 				{/* MISSÃO */}
 				<div class="row m-sm-5 justify-content-center">
-					<img class="m-3" src="./assets/icons/mission_black.svg" alt="Alvo com uma flecha" id="sobre-icon" />
+					<img
+						class="m-3"
+						src="./assets/icons/mission_black.svg"
+						alt="Alvo com uma flecha"
+						id="sobre-icon"
+					/>
 					<div class="col-lg-8 col-xl-6 col-10">
-						<h2><strong>Missão</strong></h2>
+						<h2>
+							<strong>Missão</strong>
+						</h2>
 						<p class="lead">
-							Satisfazer as necessidades do mercado com serviços e produtos de qualidade, destinados à usinagem e
-							ferramentaria, garantindo rentabilidade e atuando sempre com respeito.
+							Satisfazer as necessidades do mercado com serviços e
+							produtos de qualidade, destinados à usinagem e
+							ferramentaria, garantindo rentabilidade e atuando
+							sempre com respeito.
 						</p>
 					</div>
 				</div>
@@ -37,23 +52,37 @@ function Sobre() {
 				{/* VALORES */}
 				<div class="row m-sm-5 justify-content-center">
 					<div class="col-lg-8 col-xl-6 col-10 text-end">
-						<h2><strong>Valores</strong></h2>
+						<h2>
+							<strong>Valores</strong>
+						</h2>
 						<p class="lead">
-							Nossas relações são baseadas em: Qualidade, melhoria continua,
-							comprometimento, transparência, confiança, cooperação,
-							integridade, inovação e sustentabilidade.
+							Nossas relações são baseadas em: Qualidade, melhoria
+							continua, comprometimento, transparência, confiança,
+							cooperação, integridade, inovação e
+							sustentabilidade.
 						</p>
 					</div>
-					<img class="m-3" src="./assets/icons/value_black.svg" alt="Alvo com uma flecha" id="sobre-icon" />
+					<img
+						class="m-3"
+						src="./assets/icons/value_black.svg"
+						alt="Alvo com uma flecha"
+						id="sobre-icon"
+					/>
 				</div>
 
 				{/* Fale conosco */}
 				<div class="row pt-5 m-3 justify-content-evenly">
 					<h3 class="col-sm-6 col-lg-4">
-						Tem alguma dúvida? Quer conhecer nosso método de trabalho?
+						Tem alguma dúvida? Quer conhecer nosso método de
+						trabalho?
 					</h3>
-					<a href="/contato" class="btn text-white rounded col-sm-6 col-lg-3 align-content-center"
-						id="sobre-btn-contato">Envie uma mensagem</a>
+					<a
+						href="/contato"
+						class="btn text-white rounded col-sm-6 col-lg-3 align-content-center"
+						id="sobre-btn-contato"
+					>
+						Envie uma mensagem
+					</a>
 				</div>
 			</div>
 
