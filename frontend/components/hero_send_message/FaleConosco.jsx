c4e9f80810051728b37e3f5039ec00da01961fd0
Gustavo Balbino <gu_gui14@hotmail.com>
2025-04-16 16:31:43 -0300

    fix(faleconosco): responsividade do botao envie uma mensagem

--- frontend/components/hero_send_message/FaleConosco.jsx
+++ frontend/components/hero_send_message/FaleConosco.jsx
@@ -4,11 +4,11 @@ function FaleConosco({ message }) {
 	return (
 		<>
 			{/* HERO "Envie uma mensagem" */}
-			<div class="row py-5 my-5 justify-content-evenly">
+			<div class="row py-5 my-md-5 justify-content-evenly">
 				<h3 class="col-10 col-md-6 col-sm-6 col-lg-4">{message}</h3>
 				<a
 					href="/contato"
-					class="btn text-white rounded col-sm-6 col-lg-3 align-content-center"
+					class="btn text-white rounded col-8 col-sm-4 col-lg-3 align-content-center"
 					id="btn-contato"
 				>
 					Envie uma mensagem
